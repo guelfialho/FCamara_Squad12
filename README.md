2adc88b95cfd10cd41405a23a1c08c9ec4608a1a
Wellington Henrique <wellingtonhlc@gmail.com>
2022-04-06 21:48:58 -0300

    Adding basic project structure

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# FCamara_Squad12
+Projeto do squad 12 no hackathon do Programa de Formação do Grupo FCamara.
+
+## Dependencies
+### `node`  `express`  `mongoose` `dotenv` `react` `react-router-dom` `axios`
+
+## Starting Front-end project
+**Inicie o projeto Front-end separadamente do Back-end**
+### `npm i` 
+### `npm start` 
+
+**Have a fun!**
---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props => 
+    <Main icon="home" title="Início" subtitle="Vamos definir uma mensagem aqui...">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Encontre seus mentores e tire as suas dúvidas!</p>
+    </Main>
+
+    
